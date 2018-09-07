d0b7223355e2f5d141919486d1a361e2e22dda3b
Jun Salinga <jsalinga@gmail.com>
2018-09-07 18:18:56 +0800

    Remove field uppercase conversion

--- one/models/res_partner.py
+++ one/models/res_partner.py
@@ -15,29 +15,29 @@ class FasResPartner(models.Model):
   fax = fields.Char(string="Fax")
   over_credit = fields.Boolean('Allow Over Credit?')
 
-  @api.model
-  def create(self, values):
-    to_lower_list = ['email','website','company_type','picking_warn','invoice_warn', 'lang', 'type', 'purchase_warn', 'sale_warn', 'trust']
-    for v in values:
-      if type(values[v]) is str:
-        if not v in to_lower_list:
-          values[v] = str(values[v]).upper()
-        else:
-          logger.info("Variable Name: "+ str(v) + " Type: " + str(type(values[v])))
-    record = super(FasResPartner, self).create(values)
-    return record
+#  @api.model
+#  def create(self, values):
+#    to_lower_list = ['tz','email','website','company_type','picking_warn','invoice_warn', 'lang', 'type', 'purchase_warn', 'sale_warn', 'trust']
+#    for v in values:
+#      if type(values[v]) is str:
+#        if not v in to_lower_list:
+#          values[v] = str(values[v]).upper()
+#        else:
+#          logger.info("Variable Name: "+ str(v) + " Type: " + str(type(values[v])))
+#    record = super(FasResPartner, self).create(values)
+#    return record
 
-  @api.multi
-  def write(self, values):
-    to_lower_list = ['email','website','company_type','picking_warn','invoice_warn', 'lang', 'type', 'purchase_warn', 'sale_warn', 'trust']
-    for v in values:
-      if type(values[v]) is str:
-        if v in to_lower_list:
-          values[v] = str(values[v]).lower()
-        else:
-          values[v] = str(values[v]).upper()
+#  @api.multi
+#  def write(self, values):
+#    to_lower_list = ['tz','email','website','company_type','picking_warn','invoice_warn', 'lang', 'type', 'purchase_warn', 'sale_warn', 'trust']
+#    for v in values:
+#      if type(values[v]) is str:
+#        if v in to_lower_list:
+#          values[v] = str(values[v]).lower()
+#        else:
+#          values[v] = str(values[v]).upper()
       #else:
       #  logger.info("Variable Name: "+ str(v) + " Type: " + str(type(values[v])))
-    super(FasResPartner, self).write(values)
+#    super(FasResPartner, self).write(values)
   
 FasResPartner()
