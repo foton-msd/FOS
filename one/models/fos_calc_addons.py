6de89fbb5a15b11e00994ef38d85a4b4e8fc0671
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-01-03 07:58:05 +0800

    fixed issue on the amount when saving addons

--- one/models/fos_calc_addons.py
+++ one/models/fos_calc_addons.py
@@ -10,7 +10,7 @@ class FOSCalcAddons(models.Model):
   fos_calc_id = fields.Many2one(string="FOS Calculator", comodel_name="fos.calc")
   product_id = fields.Many2one(string='Addons', comodel_name='product.product')
   description = fields.Text(string='Description', related="product_id.product_tmpl_id.description")
-  amount = fields.Float('Amount', digits=dp.get_precision('Product Price'), default=0.0, related="product_id.product_tmpl_id.list_price")
+  amount = fields.Float('Amount', digits=dp.get_precision('Product Price'), default=0.0)
     
   @api.onchange("product_id")
   def amount_changed(self):
