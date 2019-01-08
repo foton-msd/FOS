257ebd768aec7ecfd8e7a34a980f11e6e7f90238
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-01-08 10:57:36 +0800

    create an api, that will get the price of the labor codes in local name module

--- one/models/sale_order_line.py
+++ one/models/sale_order_line.py
@@ -12,12 +12,14 @@ class FasSaleOrderLine(models.Model):
     selection=[('warranty','Warranty'),
       ('customer','Customer'),
       ('internal','Internal'),
+      ('first_pms','First PMS'),
+      ('tsb','TSB'),
       ('add-on-costs','Add-On-Costs')])
   fu_id = fields.Many2one(string="FOTON Number", comodel_name="one.fu")
-  one_charged_to = fields.Selection(string="Charged to", 
-    selection=[('warranty','Warranty'),
-      ('customer','Customer'),
-      ('internal','internal')])
+#  one_charged_to = fields.Selection(string="Charged to", 
+#    selection=[('warranty','Warranty'),
+#      ('customer','Customer'),
+#      ('internal','internal')])
   discount_amount = fields.Float(string="Discount (Amount)", digits=dp.get_precision('Discount'))
   part_number = fields.Many2one(string="Part Number", comodel_name="product.product")
   parts_and_jobs = fields.Many2one(string="Parts & Labor", comodel_name="product.product")
@@ -45,19 +47,34 @@ class FasSaleOrderLine(models.Model):
   def OnChangedPartsAndLabor(self):
     self.product_id = self.parts_and_jobs
     self.product_id_change()
+    if self.product_id:
+      lc_obj = self.env['fos.labor.codes'].browse([
+        ('one_local_name_id','=',self.order_id.fu_local_name_id.id)
+        ])
+      if lc_obj:
+        for lc1 in lc_obj:
+          self.product_uom_qty = lc1.no_of_hours
+          pp_obj = self.env['product.product'].search(['id','=',self.product_id.id])
+          if pp_obj:
+            for pp in pp_obj:
+              pp.write({'list_price': lc1.flat_rate})
+          self.price_unit = lc1.flat_rate
 
+          logger.info("flat_rate:" + str(lc1.flat_rate))
+          logger.info("price_unit:" + str(self.price_unit))
+      
   @api.onchange("units_and_addons")
   @api.depends("product_id")
   def OnChangedUnitsAndAddons(self):
     self.product_id = self.units_and_addons
     self.product_id_change()
 
-  @api.onchange("one_charged_to")
-  @api.depends("charged_to")
-  def oneChargedToChanged(self):
+#  @api.onchange("one_charged_to")
+#  @api.depends("charged_to")
+#  def oneChargedToChanged(self):
     #if self.one_charged_to:
-    self.charged_to = self.one_charged_to or 'warranty'
-    logger.info("Charged to: " + self.charged_to)  
+#    self.charged_to = self.one_charged_to or 'warranty'
+#    logger.info("Charged to: " + self.charged_to)  
 
   @api.multi
   def name_get(self):
