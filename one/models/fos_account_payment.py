9667e2a2e79aab2c168db13761f9375c01750f68
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-03-20 12:37:40 +0800

    additional field payment_received_from

--- one/models/fos_account_payment.py
+++ one/models/fos_account_payment.py
@@ -7,5 +7,11 @@ class FosAccountPayment(models.Model):
   
   note = fields.Text('Notes')
   cheque_number = fields.Char('Cheque Number')
+  payment_received_from = fields.Char(string="Payment Received from")
+
+  @api.onchange("partner_id")
+  def partner_changed(self):
+      if self.partner_id:
+        self.payment_received_from = self.partner_id.name
 
 FosAccountPayment()
