a51f75824f8bcd2643f5d47da61794c756869428
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-08-05 09:34:06 +0800

    revert last changes - restore old code -  08-04-19

--- one/report/fos_vqir_report.py
+++ one/report/fos_vqir_report.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*- 
 from odoo import tools 
 from odoo import api, fields, models
-from odoo.addons import decimal_precision as dp
 
 class VqirReport1(models.Model):
-  _name = 'view.fos.vqir.report1' 
-  _description = 'VQIR Report-1'
+  _name = 'view.fos.vqir.report' 
+  _description = 'VQIR Report'
   _auto = False
 
-  name = fields.Char(string="ID", readonly=True)
-  fmpi_vqir_id = fields.Many2one(string="V.Q.I.R.", comodel_name="fmpi.vqir", readonly=True)
-  si_number = fields.Char(string="S.I. Number", readonly=True)
-  si_date = fields.Date(string="S.I. Date", readonly=True)
-  vqir_date = fields.Date(string="VQIR Date", readonly=True)
-  parts_number = fields.Char(string="Parts Number", readonly=True)
-  parts_desc = fields.Char(string="Parts Description", readonly=True)
-  parts_qty = fields.Float(string="Quantity", readonly=True)
-  parts_cost = fields.Float(string="U/Price", readonly=True)
-  #parts_with_fee = fields.Boolean(string="With 10% handling fee", readonly=True)
+  name = fields.Char(string="VQIR Number")
+  vqir_date = fields.Datetime(string="VQIR Date")
+  fos_fu_id = fields.Many2one(string="FOTON Number", comodel_name="one.fu")
+  fu_chassis_number = fields.Char(string="Chassis Number", related="fos_fu_id.chassis_number")
+  fu_engine_number = fields.Char(string="Engine Number", related="fos_fu_id.engine_number")
+  users_name = fields.Char(string="User's Name")
+  users_mobile = fields.Char(string="Phone")
+  run_km = fields.Integer(string="Run KM")
+  date_occur = fields.Date(string="Date of occurence")
+  km_1st_trouble = fields.Integer(string="KM of first trouble happened")
+  trouble_cause_analysis = fields.Text(string="Trouble Cause Analysis")
+  disposal_measures = fields.Text(string="Disposal Measures")
+  date_occur = fields.Date(string="Date of occurence")
+  parts_number = fields.Char(string="Part Number")
+  parts_desc = fields.Char(string="Parts Description")
+  parts_qty = fields.Float(string="Quantity")
+  parts_cost = fields.Float(string="U/Price")
   parts_hf_amount = fields.Float(string="HF Amount")
-  parts_total = fields.Float(string="Parts Net Total", readonly=True)
-  job_code = fields.Char(string="Job Code", readonly=True)
-  job_code_desc = fields.Char(string="Job Desc", readonly=True)
-  job_qty = fields.Float(string="Job Qty", readonly=True)
-  job_cost = fields.Float(string="Job Cost", digits=dp.get_precision('Product Price'), readonly=True)
-  #job_total = fields.Float(string="Job Total", compute="_getJobTotal", readonly=True)
-  #job_parts_total = fields.Float(string="Total", compute="_getJobPartsTotal", readonly=True)
-  approved_amount = fields.Float(string="Parts Approved Amount")
-  part_id = fields.Many2one(string="Part Number", comodel_name="product.product", readonly=True)
-  job_id = fields.Many2one(string="Job Code", comodel_name="product.product", readonly=True)
-  job_approved_amount = fields.Float(string="Job Approved Amount")
-  #job_parts_approved_total = fields.Float(string="Total", compute="_getJobPartsApprovedTotal", readonly=True)
-  dealer_pj_id = fields.Integer(string="Dealer PJ ID")
+  parts_total = fields.Float(string="Parts Total")
+  job_code = fields.Char(string="Job Code")
+  job_code_desc = fields.Char(string="Job Desc")
+  job_qty = fields.Float(string="Job Qty")
+  job_cost = fields.Float(string="Job Cost")
+  job_total = fields.Float(string="Job Total")
+  job_parts_total = fields.Float(string="Total")
+  remarks = fields.Char(string="Remarks")
   dealer_id = fields.Many2one(string="Dealer", comodel_name="one.dealers")
   vqir_state = fields.Selection(string="Status", 
     selection=[('draft', 'Draft'),('cancel', 'Cancelled'),('submit', 'Submitted'),
@@ -39,13 +40,27 @@ class VqirReport1(models.Model):
 
   @api.model_cr
   def init(self):
-      tools.drop_view_if_exists(self.env.cr, "fmpi_vqir_report1")
+      tools.drop_view_if_exists(self.env.cr, "fmpi_vqir_report")
       self.env.cr.execute("""
             CREATE OR REPLACE VIEW view_fos_vqir_report AS
-      select a.*,b.id as dealer_id, b.vqir_state, b.vqir_date
-      from fmpi_vqir_parts_and_jobs a
-      left join fmpi_vqir b on a.fmpi_vqir_id = b.id
+            SELECT a.id, a.name, a.vqir_date, a.fos_fu_id,
+                          b.chassis_number, b.engine_number, a.users_name,
+                          a.users_mobile, a.km_1st_trouble, a.run_km, a.date_occur,
+                          a.trouble_cause_analysis, a.disposal_measures, d.job_code_desc, d.job_code,
+                          d.parts_number, d.parts_desc, d.parts_with_fee, d.parts_qty, d.parts_cost,
+                          ((d.parts_qty * d.parts_cost) * case when d.parts_with_fee then 1.1 else 1 end)::float as parts_total,
+                          ((d.parts_cost * d.parts_qty) * 0.1)::float as parts_hf_amount,
+                          d.job_qty, d.job_cost, (d.job_qty * d.job_cost)::float as job_total, 
+    
+              (((d.parts_qty * d.parts_cost) * case when d.parts_with_fee then 1.1 else 1 end)::float +
+              (d.job_qty * d.job_cost)::float)::float as job_parts_total,
 
-      where not b.id isnull;
+                          b.fmpi_fu_local_name_id, a.remarks,
+                          a.ss_name, a.dealer_id, a.vqir_state,
+                          c.id AS one_fu_local_name_id, d.id AS ovpj_id
+                          FROM fmpi_vqir a
+                          LEFT JOIN one_fu b ON a.fos_fu_id = b.id
+                          LEFT JOIN one_local_names c ON b.fmpi_fu_local_name_id = c.id
+                          LEFT JOIN fmpi_vqir_parts_and_jobs d on a.id = d.fmpi_vqir_id;
                """)
 VqirReport1()
