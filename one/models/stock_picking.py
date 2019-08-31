df542c29d4f7ed48651f1dafb95156f4ba51b65f
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-08-31 15:37:52 +0800

    create print report for stock picking

--- one/models/stock_picking.py
+++ one/models/stock_picking.py
@@ -1,8 +1,19 @@
-from odoo import models, fields, api
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+  _inherit = 'stock.picking'
 
-    picking_type_code = fields.Char(string="Code", related="picking_type_id.code", readonly=True)
+  total_product_qty = fields.Float(string="Total Qty", compute="_getTotalQty", digits=dp.get_precision('Product Unit of Measure'))
+
+  @api.one
+  def _getTotalQty(self):
+    for line in self.move_line_ids:
+       self.total_product_qty += line.qty_done
+       
+  @api.multi
+  def print_picking_list(self):
+    return self.env.ref('one.report_picking_list').report_action(self) 
 
 StockPicking()
