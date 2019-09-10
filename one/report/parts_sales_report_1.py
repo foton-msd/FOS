fb2a6bf0575da04aba5252b3e1ad00ef40cb31aa
Jay-Ar Maderas <jmaderas@foton.com.ph>
2019-09-10 23:36:22 +0800

    modify the sale parts report

--- one/report/parts_sales_report_1.py
+++ one/report/parts_sales_report_1.py
@@ -7,14 +7,18 @@ class PartsSalesReport1(models.Model):
   _description = 'Sales Report-1'
   _auto = False
 
-  product_id = fields.Many2one(string="Part Number", comodel_name="product.product")
-  name = fields.Char(string="Part Number", related="product_id.product_tmpl_id.name")
-  confirmation_date = fields.Datetime(string="Confirmation Date")
-  partner_id = fields.Many2one(string="Customer", comodel_name="res.partner")
-  order_id = fields.Many2one(String="S.O. Number", comodel_name="sale.order")
-  product_uom_qty = fields.Float(string="Order Qty")
-  price_unit = fields.Float(string="Unit Price")
-  price_subtotal = fields.Float(string="Order Total")
+  part_number = fields.Char(string="Part Number", readonly=True)
+  description = fields.Char(string="Description", readonly=True)
+  confirmation_date = fields.Datetime(string="Confirmation Date", readonly=True)
+  customer_name = fields.Char(string="Customer", readonly=True)
+  source_code = fields.Char(string="SC", readonly=True)
+  so_number = fields.Char(String="S.O. Number", readonly=True)
+  polo_number = fields.Char(string="Source Document", readonly=True)
+  order_qty = fields.Float(string="Order Qty", readonly=True)
+  qty_unserved = fields.Float(string="Unserved Qty", readonly=True)
+  qty_delivered = fields.Float(string="Qty Delivered", readonly=True)
+  price_unit = fields.Float(string="Unit Price", readonly=True)
+  price_total = fields.Float(string="Order Total", readonly=True)
   charged_to = fields.Selection(string="Charged to", required=True,
     selection=[('warranty','Warranty'),
       ('customer','Customer'),
@@ -26,10 +30,30 @@ class PartsSalesReport1(models.Model):
     tools.drop_view_if_exists(self.env.cr, "view_parts_sales_report_1")
     self.env.cr.execute("""
       CREATE OR REPLACE VIEW view_parts_sales_report_1 AS
-        SELECT a.*, b.partner_id, b.confirmation_date, b.sq_number, b.name AS so_number 
-        FROM sale_order_line a
-        LEFT JOIN sale_order b ON a.order_id = b.id
-        WHERE b.state in ('sale','done') AND b.so_type = 'parts'
+      SELECT e.name AS source_code,
+        c.name AS part_number,
+        c.description,
+        a.product_uom_qty AS order_qty,
+        a.qty_delivered,
+        a.product_uom_qty - a.qty_delivered AS qty_unserved,
+        f.name AS so_number,
+        f.origin AS polo_number,
+        f.confirmation_date,
+        g.name AS customer_name,
+        a.charged_to,
+        a.price_total,
+        a.price_unit,
+        a.id,
+        a.product_id,
+        f.id AS order_id,
+        g.id AS partner_id
+      FROM sale_order_line a
+        LEFT JOIN product_product b ON a.product_id = b.id
+        LEFT JOIN product_template c ON b.product_tmpl_id = c.id
+        LEFT JOIN product_attribute_value_product_product_rel d ON d.product_product_id = b.id
+        LEFT JOIN product_attribute_value e ON d.product_attribute_value_id = e.id
+        LEFT JOIN sale_order f ON a.order_id = f.id
+        LEFT JOIN res_partner g ON f.partner_id = g.id
+      WHERE f.state in ('sale','done') AND f.so_type = 'parts';
         """)
-
 PartsSalesReport1()
